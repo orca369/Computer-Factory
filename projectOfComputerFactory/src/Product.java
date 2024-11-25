f3f41a779bdafe729e6b5f558bd28bf8c2b6f613
Abdelaziz shahin <abd91business@gmail.com>
2024-11-26 00:50:22 +0100

    creating sub-classes

--- projectOfComputerFactory/src/Product.java
+++ projectOfComputerFactory/src/Product.java
@@ -4,4 +4,28 @@ public class Product {
     private int width ;
     private int height ;
     private int depth ;
+
+    public Product(String model, String manufacturer) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+    }
+    class Monitor extends Product {
+
+        public Monitor(String model, String manufacturer) {
+            super(model, manufacturer);
+        }
+    }
+    class MotherBoard extends Product {
+
+        public MotherBoard(String model, String manufacturer) {
+            super(model, manufacturer);
+        }
+    }
+    class ComputerCase extends Product {
+
+        public ComputerCase(String model, String manufacturer) {
+            super(model, manufacturer);
+        }
+    }
+    // all three classes are part of the main class product
 }
